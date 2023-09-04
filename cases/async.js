dd9f6c767a13702e393024b89c1a2bdd21161f73
ar00tem <mushket.1998@mail.ru>
2023-09-04 15:26:33 +0300

    async load accordion

--- cases/async.js
+++ cases/async.js
@@ -1,57 +1,22 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-  <link rel="stylesheet" href="style.css">
-</head>
+document.addEventListener('DOMContentLoaded', () => {
+  const btn = document.querySelector('[data-load-accordions]')
+  const accordionsBlock = document.querySelector('[data-blocks]')
 
-<style>
-  /* Style the tab */
-.tab {
-  overflow: hidden;
-  border: 1px solid #ccc;
-  background-color: #f1f1f1;
-}
+  btn.addEventListener('click', () => {
+    fakeLoad()
+      .then(data => {
+        const div = document.createElement('div')
+        div.innerHTML = data
+        accordionsBlock.append(div)
+      
+        new AccordionDefined({...window.defaultAccordionSettings, context: div})
+      })
+  })
 
-/* Style the buttons that are used to open the tab content */
-.tab button {
-  background-color: inherit;
-  float: left;
-  border: none;
-  outline: none;
-  cursor: pointer;
-  padding: 14px 16px;
-  transition: 0.3s;
-}
+  async function fakeLoad() {
+    const accordionHtml = `
+      <h2>Загруженный аккордион: ${accordionsBlock.children.length}</h2>
 
-/* Change background color of buttons on hover */
-.tab button:hover {
-  background-color: #ddd;
-}
-
-/* Create an active/current tablink class */
-.tab button.active {
-  background-color: #ccc;
-}
-
-/* Style the tab content */
-.tabcontent {
-  display: none;
-  padding: 6px 12px;
-  border: 1px solid #ccc;
-  border-top: none;
-}
-</style>
-<body>
-  <div class="container">
-    <div class="tab">
-      <button class="tablinks" onclick="openCity(event, 'tab1')" id="default">tab1</button>
-      <button class="tablinks" onclick="openCity(event, 'tab2')">tab2</button>
-    </div>
-    <div id="tab1" class="tabcontent">
       <div class="accordion" data-accordion-block>
         <div class="accordion-item" data-accordion>
           <!-- second lvl -->
@@ -139,49 +104,8 @@
           </div>
         </div>
       </div>
-    </div>
-    <div id="tab2" class="tabcontent">
-      <div class="accordion" data-accordion-block>
-        <div class="accordion-item" data-accordion>
-          <div class="accordion-item__header" data-accordion-btn>
-            <h2>lorem 1</h2>
-            <div class="accordion-item__header-icon">
-              <svg class="" width="15" height="9" viewBox="0 0 15 9" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M14 1.43408L7.5 7.56616L1 1.43408" stroke="currentColor" stroke-width="2"></path></svg>
-            </div>
-          </div>
-          <div class="accordion-item__body" data-accordion-content>
-            <p class="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, magni repudiandae. Perspiciatis deleniti alias modi, dicta dolorem sed quidem consequuntur.</p>
-          </div>
-        </div>
-        <div class="accordion-item" data-accordion>
-          <div class="accordion-item__header" data-accordion-btn>
-            <h2>lorem 2</h2>
-            <div class="accordion-item__header-icon">
-              <svg class="" width="15" height="9" viewBox="0 0 15 9" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M14 1.43408L7.5 7.56616L1 1.43408" stroke="currentColor" stroke-width="2"></path></svg>
-            </div>
-          </div>
-          <div class="accordion-item__body" data-accordion-content>
-            <p class="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, magni repudiandae. Perspiciatis deleniti alias modi, dicta dolorem sed quidem consequuntur.</p>
-            <p class="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, magni repudiandae. Perspiciatis deleniti alias modi, dicta dolorem sed quidem consequuntur.</p>
-            <p class="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, magni repudiandae. Perspiciatis deleniti alias modi, dicta dolorem sed quidem consequuntur.</p>
-          </div>
-        </div>
-        <div class="accordion-item" data-accordion>
-          <div class="accordion-item__header" data-accordion-btn>
-            <h2>lorem 3</h2>
-            <div class="accordion-item__header-icon">
-              <svg class="" width="15" height="9" viewBox="0 0 15 9" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M14 1.43408L7.5 7.56616L1 1.43408" stroke="currentColor" stroke-width="2"></path></svg>
-            </div>
-          </div>
-          <div class="accordion-item__body" data-accordion-content>
-            <p class="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, magni repudiandae. Perspiciatis deleniti alias modi, dicta dolorem sed quidem consequuntur.</p>
-            <p class="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, magni repudiandae. Perspiciatis deleniti alias modi, dicta dolorem sed quidem consequuntur.</p>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-  <script src="cases/tabs.js"></script>
-  <script src="accordion.js"></script>
-</body>
-</html>
+    `
+
+    return Promise.resolve(accordionHtml)
+  }
+})
